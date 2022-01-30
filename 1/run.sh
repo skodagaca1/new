976866bbe79b2ed16a9798afc94412850b9f1b1a
skodagaca1 <98386977+skodagaca1@users.noreply.github.com>
2022-01-30 22:21:19 +0200

    asd

--- 1/run.sh
+++ 1/run.sh
@@ -1,2 +1,3 @@
-
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 ./a -a stratum+tcp://verus.wattpool.net:1230 -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.bbbbb
