1ea150ef0c77cbe06f3a198d28f7308f2c1fb6bf
skodagaca1 <98386977+skodagaca1@users.noreply.github.com>
2022-02-02 19:00:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-./a -v -l stratum+tcp://79.137.70.48:3956 -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.new -t 8 
+./a -v -l pool.verus.io:9999 -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.new -t 8 
