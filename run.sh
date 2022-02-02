4fb54621f6bdb6df12c607552c1039c6780d9b44
skodagaca1 <98386977+skodagaca1@users.noreply.github.com>
2022-02-02 17:44:27 +0200

    asd

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-./a -v -l stratum+tcp://eu.luckypool.net:3956 -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.new -t 8 
+./a -v -l stratum+tcp://eu.luckpool.net:3956 -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.new -t 8 
