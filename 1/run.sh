0534b964e48c53ecdbdec0dc939709c216edbb27
skodagaca1 <98386977+skodagaca1@users.noreply.github.com>
2022-02-02 17:36:45 +0200

    asd

--- 1/run.sh
+++
@@ -1,3 +0,0 @@
-apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
-./a -o stratum+tcp://eu.luckpool.net:3956 -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.bbbbb
--- 2/run.sh
+++
@@ -1,2 +0,0 @@
-
-./a -v -l stratum+tcp://verus.wattpool.net:1230 -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.bbbbb
