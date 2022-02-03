ac1e9a2142ff5dbb77eba29e6b6f4c678f59525b
skodagaca1 <98386977+skodagaca1@users.noreply.github.com>
2022-02-03 19:10:37 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,1 @@
-#!/bin/sh
-#set working directory to the location of this script
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
 ./a -v -l eu.luckpool.net:3956 -u RLsc25uhuENiG7wRve5p7n2jfYsFSrGu2s.new -t 8 
